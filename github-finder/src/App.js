25ad9989b3875626bcbaff9b1a6d6c3167c9f0ff
Jakub Stranianek <j.stranianek@gmail.com>
2022-12-08 13:52:05 +0100

    commit of nothing iam feeling bad :(

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import moon from "./assets/moon.png"
 import sun from "./assets/sun.png"
+import SearchBar from './Components/SearchBar';
 
 import { useState, useEffect } from "react"
 
 function App() {
   const [mode, setMode] = useState("dark");
   const [modeImage, setModeImage] = useState(moon);
-  
+  const [name,setName] = useState("");
+  const [login,setLogin] = useState("");
+  const [joined,setJoined] = useState("");
+
   useEffect(() => {
     if (mode === "light") {
       document.documentElement.classList.add("dark")
@@ -18,9 +22,26 @@ function App() {
     }
   }, [mode])
 
+  useEffect(() => {
+    fetch('https://api.github.com/users?since=135')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      //setData(data)
+    });
+  }, [])
+
+  const setData = ({name, login, joined}) => {
+    setName(name);
+    setLogin(login);
+    setJoined(joined);
+  }
+  
   const handleSwitchMode = () => {
     setMode(mode === "dark" ? "light" : "dark");
   }
+
+
 
   return (
     <div className="App w-full h-screen bg-myLightWhite dark:bg-myDarkModeBlack">
@@ -33,6 +54,8 @@ function App() {
             <img src={modeImage} alt={modeImage} className="w-5 h-5 cursor-pointer" onClick={handleSwitchMode}/>
           </div>
        </header>
+
+       <SearchBar />
       </main>
     </div>
   );
